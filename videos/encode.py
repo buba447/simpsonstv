3d21677b1279f8ff6647329bf0ce3bc30f3d31c2
jakest <jakestaby+gh@gmail.com>
2021-09-08 17:28:17 -0600

    Change scale for width divisble by 2 error
    The current ffmpeg can generate a width not divisible by 2 error. This resolves that error.

--- videos/encode.py
+++ videos/encode.py
@@ -29,6 +29,6 @@ for filepath in newFiles:
 	o = os.path.join(destinationDirectory, newFile)
 	if os.path.isfile(o):
 		continue
-	encodeCommand = 'ffmpeg -i "%s" -vf scale=-1:480 -c:v libx264 -profile:v baseline -level 3.0 -preset fast -crf 23 -pix_fmt yuv420p "%s"' % (i, o)
+	encodeCommand = 'ffmpeg -i "%s" -vf scale=-2:480 -c:v libx264 -profile:v baseline -level 3.0 -preset fast -crf 23 -pix_fmt yuv420p "%s"' % (i, o)
 	print('Encoding %s' % newFile)
 	encode = os.popen(encodeCommand).read()
